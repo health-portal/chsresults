070e312173f44f605d1266b7a0b7243bf8191b4a
Victor Momodu <victoramomodu@gmail.com>
2025-09-15 16:58:57 +0100

    feat: add role, department and faculty tables

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,8 +1,35 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { uuid, pgTable, varchar } from 'drizzle-orm/pg-core';
 
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const staffTable = pgTable('staff', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+
+  firstName: varchar('first_name', { length: 255 }).notNull(),
+  lastName: varchar('last_name', { length: 255 }).notNull(),
+  otherName: varchar('other_name', { length: 255 }),
+});
+
+export const studentTable = pgTable('student', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  email: varchar('email', { length: 255 }).unique(),
+  matricNumber: varchar('matric_number', { length: 100 }).unique(),
+
+  firstName: varchar('first_name', { length: 255 }).notNull(),
+  lastName: varchar('last_name', { length: 255 }).notNull(),
+  otherName: varchar('other_name', { length: 255 }),
+});
+
+export const departmentTable = pgTable('department', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: varchar('name', { length: 255 }).notNull().unique(),
+});
+
+export const roleTable = pgTable('role', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: varchar('name', { length: 100 }).notNull().unique(),
+});
+
+export const facultyTable = pgTable('faculty', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: varchar('name', { length: 100 }).notNull().unique(),
 });
