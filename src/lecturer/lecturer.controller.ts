be000a5cb6085c2c0cefe28f5991e7b1baedf22c
Victor Momodu <victoramomodu@gmail.com>
2025-09-21 00:05:00 +0100

    feat: add lecturers routes and handles

--- src/lecturer/lecturer.controller.ts
+++ src/lecturer/lecturer.controller.ts
@@ -1,7 +1,77 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { LecturerService } from './lecturer.service';
 
 @Controller('lecturer')
 export class LecturerController {
   constructor(private readonly lecturerService: LecturerService) {}
+
+  @Get('courses')
+  async listCourses() {
+    return await this.lecturerService.listCourses();
+  }
+
+  @Post('courses/:courseId/students/bulk')
+  @UseInterceptors(FileInterceptor('file'))
+  async registerStudentsBulk(
+    @Param('courseId', ParseUUIDPipe) courseId: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return await this.lecturerService.registerStudentsBulk(courseId, file);
+  }
+
+  @Post('courses/:courseId/students')
+  async registerStudent(
+    @Param('courseId', ParseUUIDPipe) courseId: string,
+    @Body() body: any,
+  ) {
+    return await this.lecturerService.registerStudent(courseId, body);
+  }
+
+  @Patch('courses/:courseId/students/:studentId')
+  async updateStudent(
+    @Param('courseId', ParseUUIDPipe) courseId: string,
+    @Param('studentId', ParseUUIDPipe) studentId: string,
+    @Body() body: any,
+  ) {
+    return await this.lecturerService.updateStudent(courseId, studentId, body);
+  }
+
+  @Post('courses/:courseId/scores')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadScores(
+    @Param('courseId', ParseUUIDPipe) courseId: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return await this.lecturerService.uploadScores(courseId, file);
+  }
+
+  @Patch('courses/:courseId/scores/:studentId')
+  async editScore(
+    @Param('courseId', ParseUUIDPipe) courseId: string,
+    @Param('studentId', ParseUUIDPipe) studentId: string,
+    @Body() body: any,
+  ) {
+    return await this.lecturerService.editScore(courseId, studentId, body);
+  }
+
+  @Get('courses/:courseId/results')
+  async viewCourseResults(@Param('courseId', ParseUUIDPipe) courseId: string) {
+    return await this.lecturerService.viewCourseResults(courseId);
+  }
+
+  @Get('courses/:courseId/students')
+  async listCourseStudents(@Param('courseId', ParseUUIDPipe) courseId: string) {
+    return await this.lecturerService.listCourseStudents(courseId);
+  }
 }
--- src/lecturer/lecturer.service.ts
+++ src/lecturer/lecturer.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class LecturerService {}
+export class LecturerService {
+  constructor(private readonly db: DatabaseService) {}
+
+  async listCourses() {}
+
+  async registerStudentsBulk(courseId: string, file: Express.Multer.File) {}
+
+  async registerStudent(courseId: string, body: any) {}
+
+  async updateStudent(courseId: string, studentId: string, body: any) {}
+
+  async uploadScores(courseId: string, file: Express.Multer.File) {}
+
+  async editScore(courseId: string, studentId: string, body: any) {}
+
+  async viewCourseResults(courseId: string) {}
+
+  async listCourseStudents(courseId: string) {}
+}
