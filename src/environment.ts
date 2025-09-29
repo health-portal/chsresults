8e077f125ec77d742b93321bfa722bcbba5f8b23
Victor Momodu <victoramomodu@gmail.com>
2025-09-29 15:35:37 +0100

    chore: remove FRONTEND_BASE_URL

--- src/environment.ts
+++ src/environment.ts
@@ -6,8 +6,6 @@ dotenv.config();
 export const env = cleanEnv(process.env, {
   DATABASE_URL: str(),
   DEFAULT_ADMINS: json<{ name: string; email: string }[]>(),
-  FRONTEND_BASE_URL: str(),
   JWT_SECRET: str(),
   PORT: num(),
-  
 });
