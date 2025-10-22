696b73af4df02a420eba468777440b097ed9e6a1
Victor Momodu <victoramomodu@gmail.com>
2025-10-22 21:13:06 +0100

    chore: remove FRONTEND_BASE_URL

--- src/environment.ts
+++ src/environment.ts
@@ -7,7 +7,6 @@ export const env = cleanEnv(process.env, {
   DATABASE_URL: str(),
   JWT_SECRET: str(),
   PORT: num(),
-  FRONTEND_BASE_URL: str(),
   SMTPEXPRESS_PROJECT_ID: str(),
   SMTPEXPRESS_PROJECT_SECRET: str(),
   SMTPEXPRESS_SENDER_EMAIL: str(),
